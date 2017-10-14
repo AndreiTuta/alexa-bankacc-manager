0393b37778d44aab7aa3258c62ee4e99ad6a333b
AndreiTuta <tuta.andrei96@gmail.com>
2017-10-15 00:50:32 +0200

    Added states in the ethereum js

--- backend/chatskills/ethereum.js
+++ backend/chatskills/ethereum.js
@@ -3,33 +3,47 @@ var chatskills = require('./lib/chatskills');
 // Create a new skill.
 var ethereum = chatskills.add('ethereum');
 
-var myPythonScriptPath = '../python-prediction/run.py';
+var myPythonScriptPath = '../python-prediction/prediction.py';
 // Use python shell
 var PythonShell = require('python-shell');
 var pyshell = new PythonShell(myPythonScriptPath);
 
-var x = 100;
-
 // Create intents.
 ethereum.intent('ethereum', {
         'slots': { 'STATE': 'NUMBER' },
-        'utterances': ['{about} {ethereum} {price}']
+        'utterances': ['{for} {price}']
     },
     function(req, res) {
-        pyshell.on('message', function(message) {
-            // received a message sent from the Python script (a simple "print" statement)
-            res.say(message);
-        });
+        var state = req.get('state') || 0;
+        if (state < 3) {
+            req.set('state', state + 1);
+            res.say("At the moment,the price of ethereum is $" + 344.97 + ' .Care for an analysis?')
+            return true;
+        }
+    });
 
-        // end the input stream and allow the process to exit
-        pyshell.end(function(err) {
-            if (err) {
-                throw err;
-            };
-
-            res.say('Finished prediction');
-        });
-
+ethereum.intent('c', {
+        'slots': { 'STATE': 'NUMBER' },
+        'utterances': ["{Yes|Ok}"]
+    },
+    function(req, res) {
+        var state = req.get('state');
+        if (state < 2) {
+            req.set('state', state + 1);
+            res.say('Here we go: ');
+        }
         return true;
+
+    }
+);
+
+ethereum.intent('ethereum-buy', {
+        'slots': { 'STATE': 'NUMBER' },
+        'utterances': ["{I want to buy!} {some!}"]
+    },
+    function(req, res) {
+        if (req.get('state') == 4) {
+            res.say("Can't do that unfortunatelly.");
+        }
     }
 );
