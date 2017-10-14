d4dac7e5989744e8795103da409e942044a5b29e
AndreiTuta <tuta.andrei96@gmail.com>
2017-10-14 13:36:39 +0200

    FIXED PYTHON

--- backend/chatskills/run.js
+++ backend/chatskills/run.js
@@ -19,7 +19,7 @@ require('./bank');
 require('./dateplanner');
 require('./ethereum')
 
-console.log('\n Welcome, here are some available commands to try: \n\n * chatskills, ask hello to say hi \n * chatskills, ask dateplanner to run\n \n Banking: \n * chatskills,ask bank for balance \n * chatskills,ask bank for last balance \n * chatskills,ask bank for price of ethereum/bitcoins \n');
+console.log('\n Welcome, here are some available commands to try: \n\n * chatskills, ask hello to say hi \n * chatskills, ask dateplanner to run\n \n Banking: \n * chatskills,ask bank for balance \n * chatskills,ask bank for last balance \n \n Ethereum: \n * chatskills,ask ethereum for price \n');
 
 // Example client.
 var text = ' ';
